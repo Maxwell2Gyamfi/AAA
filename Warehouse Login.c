7f34b132cc057dbc5272c38c298174318e3b5b36
Maxwell Gyamfi <Maxwell2.Gyamfi@live.uwe.ac.uk>
2019-01-22 15:52:19 +0000

    Warehouse Login Main

--- Warehouse Login.c
+++ Warehouse Login.c
@@ -87,3 +87,96 @@ typedef struct Items_linked_list
 	struct Items_linked_list *next;
 
 }linked_list_items;
+
+int main()
+{
+	//local variables
+	int choice = 0;
+	int count = 0;
+
+	//allocate memory for user accounts
+	userdetails *users = (userdetails*)calloc(MAX_ACCOUNTS, sizeof(userdetails));
+	count = read_users_file(users, "all_users.txt");
+
+
+	do
+	{
+		system("cls");//clear screen
+		main_display_menu();
+		//action selection menu
+		choice = get_valid_integer(1, 3);
+		switch (choice)
+		{
+		case 1:
+			count == 0 ? printf("\nThere are no accounts in the system"), Pause() :
+				(count = login(users, count));
+			break;
+		case 2:
+			create_account(&count, users);
+			save_users_files(users, count, "all_users.txt");
+			Pause();
+			break;
+		default:
+			printf("\nQuitting!!!");
+			Pause();
+			break;
+		}
+	} while (choice != 3);
+	//free dynaically allocated memory
+	free(users);
+	return 0;
+}
+/**************************************************************************
+* Function Name: main_display_menu()
+*
+* Funtion Description:
+*   -This function displays the main menu of the program prompting the user
+*    to select an option between, login with email and password and create
+*    a new user account.
+*
+* User-interface variables:-
+*	*OUT (Return values):
+*			- NONE
+*	*IN (Value Parameters):
+*			- NONE
+*	*IN and OUT (Reference Parameters):
+*			- NONE
+*
+* History [Date (Author): Description)]:-
+* 2019-17-01 (Maxwell Gyamfi): displays main menu
+****************************************************************************/
+void main_display_menu()
+{
+	printf("\n\n");
+	printf("               WAREHOUSE SYSTEMS MAIN MENU\n");
+	printf("               ---------------------------\n");
+	printf("  1 ---> Login with email and password\n");
+	printf("  2 ---> Create a new account\n");
+	printf("  3 ---> Exit program\n\n");
+	printf("---> Select an option(1-3): ");
+}
+/******************************************************************************************
+* Function Name: Pause()
+*
+* Funtion Description:
+*   This function pauses the screen prompting the user to press any key to proceed
+*
+*
+* User-interface variables:-
+*	*OUT (Return values):
+*			- NONE
+*	*IN (Value Parameters):
+*			- NONE
+*	*IN and OUT (Reference Parameters):
+*			- NONE
+*
+* History [Date (Author): Description)]:-
+* 2019-17-01 (Maxwell Gyamfi): pauses screen
+******************************************************************************************/
+void Pause()
+{
+
+	printf("\n\nPress any key to continue...");
+	getch();
+
+}
